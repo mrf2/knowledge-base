894c65ca0c5f10c729a2ec8533c5f734b6c6a891
mrf2 <mrf2.github@gmail.com>
2025-07-08 20:34:43 +0600

    add functions.md and updated detecting_endianness.c

--- computing/detecing_endianness.c
+++ computing/detecing_endianness.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main(void)
 {
@@ -9,6 +10,9 @@ int main(void)
 		printf("Little-Endian\n");
 	else
 		printf("Big-Endian\n");
+
+	uint32_t x = 0x12345678;		// 32-bit integer
+	printf("Value: 0x%x\n", x);
 	
 	return 0;
 }
---
+++ development/networking/functions.md
@@ -0,0 +1,5 @@
+# functions
+|function name|meaning|purpose|library|header file|
+|---|---|---|---|---|
+|`htons()`|host to network short|convert from little-endian if need|standard C library (`libc`, `-lc`)|\<arpa/inet.h\>|
+
